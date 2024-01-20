404642623190bf1fb778d94c6a4c085cf7f86391
holly <hhfarler@gmail.com>
2024-01-20 18:45:59 +0000

    Borders finalised

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 // sketch.js
 
-// Returns a random integer between min (inclusive) and max (inclusive)
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+console.log($fx.hash);
+
+// Function to get a random element from an array using fx.rand()
+function getRandomElementFromArray(array) {
+    const index = Math.floor($fx.rand() * array.length);
+    return array[index];
+}
 
 // Define the different types of squares and their names
 let squareImages = {
@@ -32,6 +35,7 @@ let borderWidth = 31
 // Define the possible groupings
 let squares_from_left, end_squares, squares_from_bottom, squares_from_top;
 
+let initialYPos;
 let selectedSquare;
 let squareName;
 let randPick;
@@ -49,12 +53,19 @@ let squareEndingEdges = {
     br: 'right'
 };
 
-// Array to store randomness points
-let randomnessPoints = [];
-
 // Current position variables
 let currentX = borderWidth;
 let currentY = borderHeight;
+
+let sequence = [];
+
+function scaleToRangeAndRound(value, min, max) {
+    return Math.floor((value * (max - min + 1)) + min);
+}
+
+// MAKE THE RANDOMNESS!!! Randomness #1 - fx.rand()
+initialYPos = scaleToRangeAndRound($fx.rand(), 1, 7);
+console.log(initialYPos);
 
 function preload() {
     // Load images and associate them with their names
@@ -64,7 +75,6 @@ function preload() {
     for (let borderName in borderImages) {
         borderImages[borderName] = loadImage('images/' + borderName + '.png');
     }
-
 }
 
 function filterTilesByPositionAndEdge(tileGroup, currentY) {
@@ -119,6 +129,56 @@ function setup() {
             image(squareImages.empty, (i * cellDim)+borderWidth, (j * cellDim)+borderHeight);
         }
     }
+
+    // Set the starting position
+    currentY += (initialYPos - 1) * 94;
+
+    // Set the starting tile group
+    let tileGroup = squares_from_left;
+
+    // Populate the sequence array with information about each square in the sequence
+    while (currentX < (94 * 9) + borderWidth) {
+        chooseFrom = filterTilesByPositionAndEdge(tileGroup, currentY);
+
+        // RANDOMNESS #2 (*however many times) - getRandomElement uses fx.rand()
+        // Randomly select an initial square and add it to the sequence array
+        selectedSquare = getRandomElementFromArray(chooseFrom);
+        squareName = Object.keys(squareImages).find(key => squareImages[key] === selectedSquare);
+
+        sequence.push({
+            square: selectedSquare,
+            x: currentX,
+            y: currentY,
+            endingEdge: squareEndingEdges[squareName],
+            tileGroup: tileGroup
+        });
+
+        // Update the current position based on the ending edge of the last tile
+        switch (squareEndingEdges[squareName]) {
+            case 'right':
+                currentX += 94; // Move one square to the right
+                break;
+            case 'top':
+                currentY -= 94; // Move one square up
+                break;
+            case 'bottom':
+                currentY += 94; // Move one square down
+                break;
+        }
+
+        // Determine the next tile group
+        switch (squareEndingEdges[squareName]) {
+            case 'right':
+                tileGroup = squares_from_left;
+                break;
+            case 'top':
+                tileGroup = squares_from_bottom;
+                break;
+            case 'bottom':
+                tileGroup = squares_from_top;
+                break;
+        }
+    }
 }
   
   function draw() {
@@ -126,85 +186,17 @@ function setup() {
     // Draw the border images
 
     // Draw the top border
-    image(borderImages.top, 0, 0);
-
+    image(borderImages.top, borderWidth, 0);
     // Draw the left border
-    image(borderImages.left, 0, borderHeight);
-
+    image(borderImages.left, 0, 0);
     // Draw the right border
-    image(borderImages.right, 9 * 94 + borderWidth, borderHeight);
-
+    image(borderImages.right, 9 * 94 + borderWidth, 0);
     // Draw the bottom border
-    image(borderImages.bot, 0, 7 * 94 + borderHeight);
+    image(borderImages.bot, borderWidth, 7 * 94 + borderHeight);
 
-    // Draw the image
-
-    // RANDOMNESS POINT 1
-    // Randomly select an initial y-position from the 7 squares height
-    let initialYPos = getRandomInt(1, 7);
-    currentY += (initialYPos-1) * 94;
-
-    // Save the random number to the randomnessPoints array
-    randomnessPoints.push({initialYPos: initialYPos});
-    console.log(randomnessPoints);
-    console.log('Current Y:', currentY);
-
-    let tileGroup = squares_from_left;
-
-    while (currentX < (94 * 9)+borderWidth) {
-
-        chooseFrom = filterTilesByPositionAndEdge(tileGroup, currentY);
-        console.log('Choose from:', chooseFrom);
-        console.log('Tile group:', tileGroup);
-
-        // RANDOMNESS POINT 2
-        // Randomly select an initial square and draw it
-        selectedSquare = random(chooseFrom);
-        squareName = Object.keys(squareImages).find(key => squareImages[key] === selectedSquare);
-        randomnessPoints.push({square: squareName})
-        console.log(randomnessPoints);
-
-        // Draw the selected square at the calculated position
-        image(selectedSquare, currentX, currentY);
-
-        // Set the ending edge of the current tile
-        lastTileEndingEdge = squareEndingEdges[squareName];
-        console.log('Last tile ending edge:', lastTileEndingEdge);
-
-        // Update the current position based on the ending edge of the last tile
-        switch (lastTileEndingEdge) {
-            case 'right':
-                currentX += 94; // Move one square to the right
-                // Stay at the same y position
-                break;
-            case 'top':
-                // Stay at the same x position
-                currentY -= 94; // Move one square up
-                break;
-            case 'bottom':
-                // Stay at the same x position
-                currentY += 94; // Move one square down
-                break;
-            // Add more cases as needed
-        }
-
-        console.log('Square name:', squareName);
-        console.log('Current X:', currentX);
-        console.log('Current Y:', currentY);
-
-        let nextTileGroup;
-        switch (lastTileEndingEdge) {
-            case 'right':
-                nextTileGroup = squares_from_left;
-                break;
-            case 'top':
-                nextTileGroup = squares_from_bottom;
-                break;
-            case 'bottom':
-                nextTileGroup = squares_from_top;
-                break;
-        }
-        tileGroup = nextTileGroup;
-        console.log('Tile group:', tileGroup);
+    // Draw the sequence of squares based on the pre-determined sequence
+    for (let i = 0; i < sequence.length; i++) {
+        let squareInfo = sequence[i];
+        image(squareInfo.square, squareInfo.x, squareInfo.y);
     }
   }
